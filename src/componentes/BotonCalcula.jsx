25d782be2eadd3713d4f151ba23a4dc422a70f45
andres <axy09179@gmail.com>
2023-08-15 14:31:42 -0500

    corrección background

--- src/componentes/BotonCalcula.jsx
+++ src/componentes/BotonCalcula.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import calcula from "../funciones/Calcular";
 import { Contexto } from "../contexto/Contexto";
 import { Datos } from "../contexto/Contexto";
 
